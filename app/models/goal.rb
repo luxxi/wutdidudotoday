8dfe6c270dfff20a5cf55f280913b7bd9cefad5d
Luka Domitrovic <luka@domitrovic.si>
2016-07-02 19:54:48 +0200

    add active scope

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,5 @@ class Goal < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :due, presence: true
+  scope :active, -> { where("due >= ?", Time.now).order(created_at: :desc) }
 end
